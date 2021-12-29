d2fd12004acb7647a9a1342e912ab59d5a3e8496
M Afzaal Afzal <afzaal1721@gmail.com>
2021-12-29 17:40:54 +0500

    done with the login page

---
+++ components/Lgoin.js
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+import { useMoralis } from 'react-moralis';
+
+export const Login = () => {
+
+	const { authenticate } = useMoralis();
+
+	return (
+		<div className='bg-black min-h-screen text-white relative'>
+			{/* <h1>I'm the Login screen</h1> */}
+
+			<div className='flex flex-col z-10 relative min-h-screen items-center justify-center'>
+				{/* Logo will go there */}
+
+				{/* LOGIN BUTTON */}
+				<button onClick={authenticate} className='py-4 px-8 bg-[#B1511B] flex items-center justify-center shadow-2xl font-bold text-3xl rounded-lg'>
+					Login
+				</button>
+			</div>
+
+			<div className='w-full h-full'>
+				{/* BACKGROUND IMAGE */}
+				<Image src={'/login/bg.jpg'} layout={'fill'} objectFit={'cover'} />
+			</div>
+
+		</div>
+	)
+}
